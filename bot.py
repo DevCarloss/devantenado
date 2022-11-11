2d7000674d50a31b2ca34df56701e93bf24f24e2
devCarlos <112131250+DevCarloss@users.noreply.github.com>
2022-11-11 08:18:19 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,10 +1,11 @@
-from news import veja
 import discord
 from discord.ext import tasks
 from news import adrenaline
 from news import olhardigital
+from news import veja
+import config
 
-# dev antenado by devcarlos & oliyan
+# dev antenado by devcarlos
 
 Intents = discord.Intents.all()
 
@@ -12,45 +13,40 @@ client = discord.Client(intents=Intents)
 
 # Discord Embeds
 
+
+async def createEmbed(news):
+    embed = discord.Embed(
+        title=f"** {news.newsTitle} **",
+        description="\n" + news.newsDescription,
+        url=news.newsLink,
+        color=discord.Color.from_rgb(204, 0, 0),
+    )
+    embed.set_image(url=news.newsImage)
+    embed.add_field(name="Fonte", value=f"{news.URL}")
+    return embed
+
 @tasks.loop(hours=24)
 
+
+
 async def embedsnews():
-  channelId = client.get_channel()
-  
-  # Veja News
-  
-  embed = discord.Embed(
-    title = "**Notícias!**",
-    description = "\n" + "Fonte: " + '\n' + veja.vejaUrl + "\n\n" + veja.vejanewsName,
-    color = discord.Color.from_rgb(255,153,0)
-  )
-  
-  embed.set_image(url=veja.vejanewsImg)
-  
-  await channelId.send(embed=embed)
-  
-  # Adrenaline News 
-  
-  embed = discord.Embed(
-         title = "**Notícias!**",
-         description = "\n" + "Fonte: " + '\n' + adrenaline.adrenalineUrl + "\n\n" + adrenaline.adrenalinenewsName + "\n\n" + adrenaline.adrenalinenewsExcerpt ,
-    color = discord.Color.from_rgb(204,0,0))
-  
-  embed.set_image(url=adrenaline.adrenalinenewsImg)
-  
-  await channelId.send(embed=embed)
-  
-  # Olhar Digital News
-  
-  embed = discord.Embed(
-         title = "**Notícias!**",
-         description = "\n" + "Fonte: " + '\n' + olhardigital.olhardigitalUrl + "\n\n" + olhardigital.olhardigitalnewsName + "\n\n" + olhardigital.olhardigitalnewsExcerpt,
-    color = discord.Color.from_rgb(255,255,255))
-  
-  embed.set_image(url=olhardigital.olhardigitalnewsImg)
-  
-  await channelId.send(embed=embed)
- 
+    channelId = client.get_channel(config.CHANNEL_ID)
+
+    # Veja News
+
+    vejaEmbed = await createEmbed(veja)
+    await channelId.send(embed=vejaEmbed)
+
+    # Adrenaline News
+
+    adrenalineEmbed = await createEmbed(adrenaline)
+    await channelId.send(embed=adrenalineEmbed)
+
+    # Olhar Digital News
+
+    olharDigitalEmebed = await createEmbed(olhardigital)
+    await channelId.send(embed=olharDigitalEmebed)
+
 
 # Client Event
 
@@ -59,4 +55,5 @@ async def embedsnews():
 async def on_ready():
   embedsnews.start()
 
-client.run("")
+
+client.run(config.BOT_TOKEN)
