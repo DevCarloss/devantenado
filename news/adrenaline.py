ef1f9949f31f81198ede581143d730b42b12a9e0
devCarlos <112131250+DevCarloss@users.noreply.github.com>
2022-11-11 08:18:49 -0300

    Update adrenaline.py

--- news/adrenaline.py
+++ news/adrenaline.py
@@ -1,20 +1,14 @@
-import requests
-from bs4 import BeautifulSoup
-
+from news.getNews import requestNews
 # Script Noticias Tecnologia adrenaline.com
 
 # Adrenaline Requests
-
-adrenalineUrl = 'https://adrenaline.com.br/noticias'
-
-adrenalineReq = requests.get(adrenalineUrl)
-
-adrenalinebs4 = BeautifulSoup(adrenalineReq.text,'html.parser')
+URL = 'https://adrenaline.com.br/noticias'
+adrenalineBs4 = requestNews('https://adrenaline.com.br/noticias')
 
 # Adrenaline News 
 
-adrenalinenewsName = adrenalinebs4.find('div',attrs={'class': "col-lg-4 post-h__image"}).find('img')['title']
-
-adrenalinenewsImg = adrenalinebs4.find('div',attrs={'class': "col-lg-4 post-h__image"}).find('img')['data-original']
-
-adrenalinenewsExcerpt = adrenalinebs4.find('p',attrs={'class': "post-h__content-sub"}).getText()
+newsDiv = adrenalineBs4.find('div',attrs={'class': "col-lg-4 post-h__image"})
+newsLink = newsDiv.find('a')['href']
+newsTitle = newsDiv.find('img')['title']
+newsImage = newsDiv.find('img')['data-original']
+newsDescription = adrenalineBs4.find('p',attrs={'class': "post-h__content-sub"}).getText()
